0032d501082adb8b015d2bbf19c9bb06c681d992
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-03-10 22:24:01 +0100

    Added tests for entity creation and settings constructor

---
+++ GameSE2/WinFormsGameTestProject/SettingsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WinFormsGameTestProject
+{
+    [TestClass]
+    public class SettingsTest
+    {
+        [TestMethod]
+        public void SettingsCreationTest()
+        {
+            Settings settings = new Settings(200,200,720,1080,30);
+
+            Assert.AreEqual(36, settings.ViewWidth, "ViewWidth test 1 incorrect");
+            Assert.AreEqual(24, settings.ViewHeight, "ViewHeight test 1 incorrect");
+
+            settings = new Settings(200,200,653,1141,30);
+
+            Assert.AreEqual(38, settings.ViewWidth, "ViewWidth test 2 incorrect");
+            Assert.AreEqual(21, settings.ViewHeight, "ViewHeight test 2 incorrect");
+        }
+    }
+}
---
+++ GameSE2/WinFormsGameTestProject/WorldTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinFormsGame.Classes;
+
+namespace WinFormsGameTestProject
+{
+    [TestClass]
+    public class WorldTest
+    {
+        private Settings _settings = new Settings(200, 200, 930, 930, 30);
+        Level _levelField = new Level(new Settings(200, 200, 930, 930, 30), 1);
+
+        [TestMethod]
+        public void LevelTests()
+        {
+            //var world = new World(new Settings(200,200,930,930,30));
+            var level = new Level(_settings, 1);
+
+            Assert.IsNotNull(level.Player, "Player not initialized");
+            Assert.IsNotNull(level.Enemies, "Enemies not initialized");
+            Assert.IsNull(level.Enemies.Find(x => x.Location == level.Player.Location), "Enemy and player share location");
+            Assert.IsFalse(level.Enemies.First().Location == level.Enemies.Last().Location, "Test bad news");
+
+            var badNews = false;
+
+            foreach (var enemy in level.Enemies)
+            {
+                if (level.Enemies.Exists(x => x != enemy && x.Location == enemy.Location))
+                    badNews = true;
+            }
+
+            Assert.IsFalse(badNews, "Duplicate locations in enemies.");        
+        }
+
+        [TestMethod]
+        public void LevelPlayerGetPerformance()
+        {
+            Assert.IsNotNull(_levelField.Player);
+        }
+
+        [TestMethod]
+        public void LevelEnemiesGetPerformance()
+        {
+            Assert.IsNotNull(_levelField.Enemies);
+        }
+    }
+}
