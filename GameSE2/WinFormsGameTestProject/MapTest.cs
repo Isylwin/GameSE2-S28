eeb418b5c3c4ff1315430490fba75bc1bc919b2c
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-03-10 22:22:58 +0100

    Added tests for location operators

--- GameSE2/WinFormsGameTestProject/MapTest.cs
+++ GameSE2/WinFormsGameTestProject/MapTest.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinFormsGame;
 using WinFormsGame.Classes.MapClasses;
 using WinFormsGame.Classes;
 
 namespace WinFormsGameTestProject
 {
     [TestClass]
-    public class MapTesting
+    public class MapTest
     {
         [TestMethod]
         public void CellCreation()
         {
-            int _horizontalCells = 200;
-            int _verticalCells = 200;
+            int _horizontalCells = 177;
+            int _verticalCells = 177;
 
             Cell[,] Cells = new Cell[_horizontalCells , _verticalCells];
 
@@ -37,7 +38,7 @@ namespace WinFormsGameTestProject
         [TestMethod]
         public void MapCreation()
         {
-            Map map = new Map(new Settings());
+            Map map = new Map(new Settings(200,200,30,930,930));
 
             Assert.AreEqual(50, map.Cells[50, 176].Location.X, "Cell in incorrect X location");
             Assert.AreEqual(174, map.Cells[67, 174].Location.Y, "Cell in incorrect Y location");
@@ -46,6 +47,20 @@ namespace WinFormsGameTestProject
             Assert.AreEqual(true, map.Cells[199, 89].IsWall, "maxX BorderCell is not a wall");
             Assert.AreEqual(true, map.Cells[106, 0].IsWall, "0Y BorderCell is not a wall");
             Assert.AreEqual(true, map.Cells[67, 199].IsWall, "maxY BorderCell is not a wall");
+        }
+
+        [TestMethod]
+        public void LocationTests()
+        {
+            var a = new Location(76,12);
+            var b = new Location(31,76);
+            var c = new Location(76,12);
+            Location d = null;
+
+            Assert.AreEqual(true, a == c, "== success incorrect");
+            Assert.AreEqual(true, a != b, "!= success incorrect");
+            Assert.AreEqual(false, b == a, "== fail incorrect");
+            Assert.AreEqual(false, d == a, "Null reference not caught");
         }
     }
 }
