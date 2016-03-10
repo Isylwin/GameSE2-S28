0817056f39e6abb7ca173afab5950d014e898d3e
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-03-10 22:17:32 +0100

    Random point creation and location comparison

--- GameSE2/GameSE2/Classes/MapClasses/Location.cs
+++ GameSE2/GameSE2/Classes/MapClasses/Location.cs
@@ -13,8 +13,8 @@ namespace WinFormsGame.Classes.MapClasses
 
 	public class Location
 	{
-		public virtual int X { get; set; }
-		public virtual int Y { get; set; }
+		public int X { get; set; }
+		public int Y { get; set; }
 
         public Location(int x, int y)
         {
@@ -22,11 +22,46 @@ namespace WinFormsGame.Classes.MapClasses
             Y = y;
         }
 
+        /// <summary>
+        /// Returns a random location within given bounds
+        /// </summary>
+        /// <param name="xMin">Minimum x-coordinate</param>
+        /// <param name="xMax">Maximum x-coordinate</param>
+        /// <param name="yMin">Minimum y-coordinate</param>
+        /// <param name="yMax">Maximum y-coordinate</param>
+	    public Location(int xMin, int xMax, int yMin, int yMax, Random rnd)
+	    {
+            //Random rnd = new Random();
+	        X = rnd.Next(xMin, xMax);
+	        Y = rnd.Next(yMin, yMax);
+	    }
+
         public override string ToString()
         {
             return "(" + X + "," + Y + ")";
         }
 
-    }
+	    public override bool Equals(object obj)
+	    {
+	        var otherLoc = obj as Location;
+
+	        return X == otherLoc.X && Y == otherLoc.Y;
+	    }
+
+	    public static bool operator ==(Location a, Location b)
+	    {
+            if (object.ReferenceEquals(a, null))
+            {
+                return object.ReferenceEquals(b, null);
+            }
+
+            return a.Equals(b);
+	    }
+
+	    public static bool operator !=(Location a, Location b)
+	    {
+	        return !(a == b);
+	    }
+	}
 }
 
