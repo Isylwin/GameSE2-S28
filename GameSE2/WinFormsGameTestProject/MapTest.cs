c96aa1e8fd78ff9cfa69713bcfa5ddc6eef5a213
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-03-15 00:37:52 +0100

    Added some map generation test code

--- GameSE2/WinFormsGameTestProject/MapTest.cs
+++ GameSE2/WinFormsGameTestProject/MapTest.cs
@@ -10,13 +10,31 @@ namespace WinFormsGameTestProject
     [TestClass]
     public class MapTest
     {
-        private Settings _settings = new Settings(99, 99, 30, 930, 930);
+        private Settings _settings = new Settings(199, 199, 30, 930, 930);
+        private Map map;
+        private Cell[,] testCells;
+
+        public MapTest()
+        {
+            map = new Map(_settings);
+
+            testCells = new Cell[_settings.HorizontalCells,_settings.VerticalCells];
+
+            for (int i = 0; i < _settings.HorizontalCells; i++)
+            {
+                for (int j = 0; j < _settings.VerticalCells; j++)
+                {
+                    testCells[i, j] = new Cell(new Location(i, j), true); //Make a map will all walls.
+                }
+            }
+        }
+        
 
         [TestMethod]
         public void CellCreation()
         {
-            int _horizontalCells = 177;
-            int _verticalCells = 177;
+            int _horizontalCells = _settings.HorizontalCells;
+            int _verticalCells = _settings.VerticalCells;
 
             Cell[,] Cells = new Cell[_horizontalCells , _verticalCells];
 
@@ -41,15 +59,13 @@ namespace WinFormsGameTestProject
         [TestMethod]
         public void MapCreation()
         {
-            Map map = new Map(_settings);
-
             Assert.AreEqual(50, map.Cells[50, 176].Location.X, "Cell in incorrect X location");
             Assert.AreEqual(174, map.Cells[67, 174].Location.Y, "Cell in incorrect Y location");
 
             Assert.AreEqual(true, map.Cells[0, 17].IsWall, "0X BorderCell is not a wall");
-            Assert.AreEqual(true, map.Cells[199, 89].IsWall, "maxX BorderCell is not a wall");
+            Assert.AreEqual(true, map.Cells[198, 89].IsWall, "maxX BorderCell is not a wall");
             Assert.AreEqual(true, map.Cells[106, 0].IsWall, "0Y BorderCell is not a wall");
-            Assert.AreEqual(true, map.Cells[67, 199].IsWall, "maxY BorderCell is not a wall");
+            Assert.AreEqual(true, map.Cells[67, 198].IsWall, "maxY BorderCell is not a wall");
         }
 
         [TestMethod]
@@ -67,21 +83,9 @@ namespace WinFormsGameTestProject
         }
 
         [TestMethod]
-        public void ClusterTests()
+        public void MapGeneratePerformance()
         {
-            //var Cells = new Cell[_settings.HorizontalCells, _settings.VerticalCells];
-
-            //for (int i = 0; i < _settings.HorizontalCells; i++)
-            //{
-            //    for (int j = 0; j < _settings.VerticalCells; j++)
-            //    {
-            //        Cells[i, j] = new Cell(new Location(i, j), true); //Make a map will all walls.
-            //    }
-            //}
-
-            //Cluster cluster = new Cluster(Cells[67,12], Cells);
-
-            //Assert.AreEqual(8, cluster.Neighbours.Count);
+            MapGenerator.MineMazeRecursiveBacktracking(_settings.MapRandom, testCells);
         }
     }
 }
